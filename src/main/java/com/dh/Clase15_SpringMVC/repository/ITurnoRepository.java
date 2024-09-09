7af2e3e011ddd11b8ed5e5fe7d31dbb7cc613a3a
adzurita <133822324+adzurita@users.noreply.github.com>
2024-09-08 21:07:21 -0500

    Update ITurnoRepository.java

--- src/main/java/com/dh/Clase15_SpringMVC/repository/ITurnoRepository.java
+++ src/main/java/com/dh/Clase15_SpringMVC/repository/ITurnoRepository.java
@@ -2,8 +2,11 @@ package com.dh.Clase15_SpringMVC.repository;
 
 import com.dh.Clase15_SpringMVC.entity.Turno;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
 public interface ITurnoRepository extends JpaRepository<Turno, Long> {
+    List<Turno> findByOdontologoIdAndFechaAndHora(Long odontologoId, LocalDate fecha, LocalTime hora);
 }
