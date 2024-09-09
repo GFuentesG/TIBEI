10e0da527f8628eb274eb5f087cca3770d1076a6
adzurita <133822324+adzurita@users.noreply.github.com>
2024-09-08 21:43:00 -0500

    Update ITurnoServicio.java
    Actualiza la interfaz del servicio de turnos para incluir los métodos de búsqueda por fecha y por odontólogo:

--- src/main/java/com/dh/Clase15_SpringMVC/service/ITurnoServicio.java
+++ src/main/java/com/dh/Clase15_SpringMVC/service/ITurnoServicio.java
@@ -1,13 +1,22 @@
 package com.dh.Clase15_SpringMVC.service;
 
+import com.dh.Clase15_SpringMVC.entity.Odontologo;
 import com.dh.Clase15_SpringMVC.entity.Turno;
+import com.dh.Clase15_SpringMVC.exception.ResourceNotFoundException;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 
 public interface ITurnoServicio {
-    //crud
-    Turno guardar(Turno turno);
-    Turno consultarPorId(Long id);
+
+    List<Odontologo> buscarOdontologosDisponibles(LocalDate fecha, LocalTime hora);
+
+    List<Turno> buscarDisponibilidadPorOdontologo(String odontologoNombre) throws ResourceNotFoundException;
+
+    Turno guardar(String odontologoNombre, String pacienteNombre, String fecha, String hora) throws Exception;
+
+    Turno consultarPorId(Long id) throws ResourceNotFoundException;
+
     List<Turno> listarTodos();
-
-
 }
