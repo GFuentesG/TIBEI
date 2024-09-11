50904c93bd0f7dccfe49a5d20a21b57ffa0ffefb
adzurita <133822324+adzurita@users.noreply.github.com>
2024-09-11 16:08:33 -0500

    Update IOdontologoRepository.java
    Corrección de metodo con turnos

--- src/main/java/com/dh/Clase15_SpringMVC/repository/IOdontologoRepository.java
+++ src/main/java/com/dh/Clase15_SpringMVC/repository/IOdontologoRepository.java
@@ -2,13 +2,16 @@ package com.dh.Clase15_SpringMVC.repository;
 
 import com.dh.Clase15_SpringMVC.entity.Odontologo;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface IOdontologoRepository extends JpaRepository<Odontologo, Long> {  //aqui se hara las operaciones crud
-                                            // con @idnetity no necesitaremos crear las tablas
-                                            //esta linea reemplaza a toda la ImplementacionOdontologo
-//    @Query("SELECT o FROM Odontologo o WHERE o.matricula = ?1")
-    Odontologo findByMatricula (String matricula);  //usando HQL, en caso no se pueda, entonces usar jdbc como la linea anterior
+public interface IOdontologoRepository extends JpaRepository<Odontologo, Long> {
+
+    // Buscar un odontólogo por su matrícula
+    Odontologo findByMatricula(String matricula);
+
+    // Buscar un odontólogo por su nombre
+    Optional<Odontologo> findByNombre(String nombre);
 }
