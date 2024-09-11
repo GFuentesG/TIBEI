2be59e349dfd021eb2f586245366093628a0e799
adzurita <133822324+adzurita@users.noreply.github.com>
2024-09-11 15:57:38 -0500

    Update BadRequestException.java
    Correción de errores en turno

--- src/main/java/com/dh/Clase15_SpringMVC/exception/BadRequestException.java
+++ src/main/java/com/dh/Clase15_SpringMVC/exception/BadRequestException.java
@@ -1,6 +1,8 @@
 package com.dh.Clase15_SpringMVC.exception;
 
-public class BadRequestException {
+public class BadRequestException extends Exception {
 
-
+    public BadRequestException(String message) {
+        super(message);
+    }
 }
