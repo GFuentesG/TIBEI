3a4421bc801b56902b2b1392ed5b9003796dd204
adzurita <133822324+adzurita@users.noreply.github.com>
2024-09-11 15:59:16 -0500

    Update ITurnoServicio.java
    Correción de errores en turno

--- src/main/java/com/dh/Clase15_SpringMVC/service/ITurnoServicio.java
+++ src/main/java/com/dh/Clase15_SpringMVC/service/ITurnoServicio.java
@@ -1,13 +1,23 @@
 package com.dh.Clase15_SpringMVC.service;
 
+import com.dh.Clase15_SpringMVC.entity.Odontologo;
 import com.dh.Clase15_SpringMVC.entity.Turno;
+import com.dh.Clase15_SpringMVC.exception.BadRequestException;
+import com.dh.Clase15_SpringMVC.exception.ResourceNotFoundException;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 
 public interface ITurnoServicio {
-    //crud
-    Turno guardar(Turno turno);
-    Turno consultarPorId(Long id);
+
+    List<Odontologo> buscarOdontologosDisponibles(LocalDate fecha, LocalTime hora) throws BadRequestException;
+
+    List<Turno> buscarDisponibilidadPorOdontologo(String odontologoNombre) throws ResourceNotFoundException, BadRequestException;
+
+    Turno guardar(String odontologoNombre, String pacienteNombre, String fecha, String hora) throws Exception;
+
+    Turno consultarPorId(Long id) throws ResourceNotFoundException;
+
     List<Turno> listarTodos();
-
-
 }
